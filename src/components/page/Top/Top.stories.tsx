cc05770e06d159d2c9395fc11a076c7eb8f261d2
Kosuke Inoue <69829047+Kosuke2000@users.noreply.github.com>
2022-02-13 15:37:59 +0900

    Initial commit

---
+++ src/components/page/Top/Top.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { TopPageView } from "./View";
+
+export default {
+  title: "Page/Top",
+  component: TopPageView,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof TopPageView>;
+
+const Template: ComponentStory<typeof TopPageView> = (args) => (
+  <TopPageView {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
