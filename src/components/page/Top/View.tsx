7b245d4dc3b6297f89f4ad6e42781b2676cfe84b
Kosuke2000 <youuchiharu@gmail.com>
2022-02-18 16:22:59 +0900

    useChartDataの実装

--- src/components/page/Top/View.tsx
+++ src/components/page/Top/View.tsx
@@ -1,12 +1,12 @@
 import { VFC } from "react";
 
-import { usePopulationData } from "@/hooks/usePopulationData";
+import { useChartData } from "@/hooks/useChartData";
 import { usePrefecture } from "@/hooks/usePrefectures";
 
 import { PrefChart } from "@/components/model/Prefecture/PrefChart";
 
 export const TopPageView: VFC = () => {
-  usePopulationData(11, "東京");
+  useChartData(11, "東京");
   const { prefectures } = usePrefecture();
 
   return (
